f3d7ad611d701e5bee3066f4e3fb4e9344888fec
frankynztein <arellanfranciree@gmail.com>
2019-03-19 18:11:33 -0500

    Mostrando data de un elemento en consola.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
-/* Manejo del DOM */
+const worldBankDataPeru = window.WORLDBANK.PER.indicators;
+
+// SHOW FIRST INDICATOR DATA IN CONSOLE
+console.log(`${worldBankDataPeru[0].indicatorName} en ${worldBankDataPeru[0].countryName}`);
+console.log(worldBankDataPeru[0].data);
