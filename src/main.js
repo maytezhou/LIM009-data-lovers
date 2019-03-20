3094a460a81c38c882af2bbb4b57009651b3e5ae
maytezhou <maytezhou14@gmail.com>
2019-03-20 13:07:06 -0500

    lista con cada objeto de data peru

--- src/main.js
+++ src/main.js
@@ -1,21 +1,35 @@
+
+
+
+
+
 const worldBankDataPeru = window.WORLDBANK.PER.indicators;
-console.log(worldBankDataPeru);
+//Retorna  un el primer objeto anonimo (el primer indicador) de los Indicadores de Perú y con su posicion dentro de un array
+console.log(worldBankDataPeru[0],[0]);
 
 
-let newArray='';
-for(i=0;i<worldBankDataPeru.length;i++){
-newArray+=worldBankDataPeru[i];
-}
-console.log(newArray);
+//Retorna una lista de todos los objetos anonimos(de todos los indicators de Peru);
+for (let i =0;i<worldBankDataPeru.length;i++){
+   console.log(worldBankDataPeru[i]);
+};
+
+
+
 
 
 // SHOW FIRST INDICATOR DATA IN CONSOLE
 console.log(`${worldBankDataPeru[0].indicatorName} en ${worldBankDataPeru[0].countryName}`);
 console.log(worldBankDataPeru[0].data);
 
+
+
 //Me retorna un objeto anomimo (un indicador completo) el cual es llamdo por su indicatorname//using find
 function labForceWom(nameOfIndicator){return nameOfIndicator.indicatorName==="Fuerza laboral con educación intermedia, mujeres (% de la fuerza laboral femenina)"};
 console.log(worldBankDataPeru.find(labForceWom));
+console.log(worldBankDataPeru[0]);
+
+
+
 
 //INTENTO FALLIDO
 //const resultado=worldBankDataPeru.find(nameOfIndicator1=>nameOfIndicator1.indicatorName==="Fuerza laboral con educación intermedia, mujeres (% de la fuerza laboral femenina");
@@ -31,8 +45,14 @@ const arrayWithStringOfIndicatorsName=worldBankDataPeru.map((worldBankDataPeru)=
 console.log(arrayWithStringOfIndicatorsName);
 
 
+//Array de Strings con la data(objeto con las propiedades años y sus valores) 
+const arrayWithStringOfIndicatorsOnlyData=worldBankDataPeru.map((worldBankDataPeru)=>worldBankDataPeru.data);
+console.log(arrayWithStringOfIndicatorsOnlyData);
+
 //IMPRIME TODOS LOS ELEMENTOS PERO CADA ELEMENTO DEL ARRAY EN UNA LINEA -ARRAY DE STRINGS (cada Indicator Name de Peru)
 const eachIndicatorNameString=arrayWithStringOfIndicatorsName.forEach(function(element){console.log(element)});
+//IMPRIME TODOS LOS ELEMENTOS PERO CADA ELEMENTO DEL ARRAY EN UNA LINEA -ARRAY DE STRINGS (cada objeto data de Peru)
+const eachDataString=arrayWithStringOfIndicatorsOnlyData.forEach(function(element1){console.log(element1)});
 
 console.log(eachIndicatorNameString);
 //Array de strings con los Indicators Code de Peru
