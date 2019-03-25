5f85371c8e6742e5523fd4f1c21d71625e583ef7
frankynztein <arellanfranciree@gmail.com>
2019-03-25 11:45:08 -0500

    Botón de reset.

--- src/data.js
+++ src/data.js
@@ -11,10 +11,6 @@
 
 
 const worldBankDataPeru = window.WORLDBANK.PER.indicators;
-const showData = document.getElementById('show-data');
-const filterIndicatorCode = document.getElementById('filter-indicator-code');
-const filterYears = document.getElementById('filter-years');
-const btnFilter = document.getElementById('btn-filter');
 
 
 
@@ -66,11 +62,6 @@ const indicatorCodeSH = (indicator) => {
   return(indicator.indicatorCode.startsWith('SH.'));
 }
 
-// ALL
-const indicatorCodeAll = (indicator) => {
-  const sectorOptions = ['SL.', 'SE.', 'HD.', 'DT.', 'IC.', 'SP.', 'MS.', 'SG.','SH.'];
-  return(indicator.indicatorCode.includes(sectorOptions));
-}
 
 // FUNCTION - FILTERED BY INDICATOR CODE
 const filterBySector = (data, sector) => {
@@ -94,7 +85,7 @@ const filterBySector = (data, sector) => {
   } else if (sector === 'SH') {
       indicatorCodes = data.filter(indicatorCodeSH)
   } else {
-      indicatorCodes = data.filter(indicatorCodeAll)
+      indicatorCodes = worldBankDataPeru;
   }
   return indicatorCodes;
   
@@ -102,16 +93,21 @@ const filterBySector = (data, sector) => {
 }
 
 
-// FILTER BUTTON
-btnFilter.addEventListener('click', () => {
-  let filterIndicatorValue = filterIndicatorCode.value;
-  //let filterByYears = filterYears.value;
-  console.log(filterIndicatorValue);
-  // filterBySector(worldBankDataPeru, filterIndicatorValue);
-  const dataFiltrada = filterBySector(worldBankDataPeru, filterIndicatorCode.value);
-  console.log(dataFiltrada);
-  // console.log(filterByYears);
-});
+// // FILTER BUTTON
+// btnFilter.addEventListener('click', () => {
+//   let filterIndicatorValue = filterIndicatorCode.value;
+//   //let filterByYears = filterYears.value;
+//   console.log(filterIndicatorValue);
+//   // filterBySector(worldBankDataPeru, filterIndicatorValue);
+//   const dataFiltrada = filterBySector(worldBankDataPeru, filterIndicatorCode.value);
+//   // console.log(filterByYears);
+//   console.log(dataFiltrada);
+//   for (let i = 0; i < dataFiltrada.length; i++) {
+//     showData.innerHTML += `
+//         <ul><li>${dataFiltrada[i].indicatorName} en ${dataFiltrada[i].countryName}</li></ul>
+//         `;
+//     }
+// });
 
 
 // SHOW FIRST INDICATOR DATA IN CONSOLE
@@ -142,17 +138,17 @@ for (let i = 0; i < worldBankDataPeru.length; i++) {
   
 // FUNCIÓN PARA MOSTRAR DATA EN DOM
 const showDataPeru = (dataPeru, dataKeysPeru, dataValuesPeru) => {
-  for (let i = 0; i < 1; i++) {
-  showData.innerHTML += `
-      <p><strong>Indicador:</strong> ${dataPeru[0].indicatorName} en ${dataPeru[0].countryName}</p>
-      `;
-  }
-  for (let i=0;i<dataKeysPeru[0].length;i++){
-      showData.innerHTML += `
-      <li>${dataKeysPeru[0][i]}: ${dataValuesPeru[0][i]}</li>
-      `
+  // for (let i = 0; i < dataPeru.length; i++) {
+  // showData.innerHTML += `
+  //     <ul><li>${dataPeru[i].indicatorName} en ${dataPeru[i].countryName}</li></ul>
+  //     `;
+  // }
+  // for (let i=0;i<dataKeysPeru[0].length;i++){
+  //     showData.innerHTML += `
+  //     <li>${dataKeysPeru[0][i]}: ${dataValuesPeru[0][i]}</li>
+  //     `
       // console.log(mostrarData(dataKeysPeru[0][i], dataValuesPeru[0][i]))
-  }
+  // }
   // console.log(dataKeysPeru[0]);
   // console.log(dataValuesPeru[0]);
 };
--- src/main.js
+++ src/main.js
@@ -32,3 +32,30 @@
 //         }
 //     };
 //     showDataPeru (worldBankDataPeru,newArrayDataKeys, newArrayDataValues);
+
+
+const showData = document.getElementById('show-data');
+const filterIndicatorCode = document.getElementById('filter-indicator-code');
+const filterYears = document.getElementById('filter-years');
+const btnFilter = document.getElementById('btn-filter');
+const btnReset = document.getElementById('btn-reset');
+
+// FILTER BUTTON
+btnFilter.addEventListener('click', () => {
+    let filterIndicatorValue = filterIndicatorCode.value;
+    //let filterByYears = filterYears.value;
+    console.log(filterIndicatorValue);
+    // filterBySector(worldBankDataPeru, filterIndicatorValue);
+    const dataFiltrada = filterBySector(worldBankDataPeru, filterIndicatorCode.value);
+    // console.log(filterByYears);
+    console.log(dataFiltrada);
+    for (let i = 0; i < dataFiltrada.length; i++) {
+      showData.innerHTML += `
+          <ul><li>${dataFiltrada[i].indicatorName} en ${dataFiltrada[i].countryName}</li></ul>
+          `;
+      }
+  });
+
+  btnReset.addEventListener('click', () => {
+      showData.innerHTML = '';
+  })
