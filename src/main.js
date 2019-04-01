f65014581a50b57a7c2ed25dce10448d4c5b4a30
maytezhou <maytezhou14@gmail.com>
2019-03-31 23:03:50 -0500

    tratando de clasificar por categoria

--- src/main.js
+++ src/main.js
@@ -1,12 +1,97 @@
 
+const arrDataWorldBankMexico=window.WORLDBANK.MEX.indicators;//[{},{},{}...]
+//console.log(arrDataWorldBankMexico);
+const arrDataWorldBankChile=window.WORLDBANK.CHL.indicators;//[{},{},{}...]
+//console.log(arrDataWorldBankChile);
+const arrDataWorldBankBrasil=window.WORLDBANK.BRA.indicators;//[{},{},{}...]
+//console.log(arrDataWorldBankBrasil);
 const arrDataWorldBankPeru = window.WORLDBANK.PER.indicators;//[{},{},{}...]
+
+
 const showInfoData = document.getElementById('show-info-data');
 const showFilteredCategory = document.getElementById('show-filtered-category');
 const showFilteredIndicatorNames = document.getElementById('show-filtered-indicator-names');
 const selectIndicatorCodeElement = document.getElementById('select-indicator-code');
+const selectCountryName = document.getElementById('select-country-name');
 const btnSearch = document.getElementById('btn-search');
 const arrUniqueInitialsCode = worldBank.getUniqueInitialsIndicatorCodeValues(worldBank.getInitialsIndicatorCodeValues(arrDataWorldBankPeru));
 console.log(arrUniqueInitialsCode);//["","",""...]
+
+
+//En el caso de que aumentara mas propeidades
+window.WORLDBANK;
+console.log(window.WORLDBANK);
+Object.keys(window.WORLDBANK);//Array ["","",""] de STR countrynames
+console.log(Object.keys(window.WORLDBANK));
+
+const objToArrObj=(obj)=>{//{}
+  const arr1=[];
+  const properties=Object.keys(obj);
+  for(let i =0;i<properties.length;i++){
+    arr1.push(obj[properties[i]])
+  }
+return arr1;
+}
+objToArrObj(window.WORLDBANK);//[{},{},{}..] Array de  Objects c/u representa un país
+console.log(objToArrObj(window.WORLDBANK));//[{},{},{}...] Array de Objects c/u representa un país
+
+
+
+
+
+
+Object.values(window.WORLDBANK);
+console.log(Object.values(window.WORLDBANK))//[{},{},{}..] c/u objc representa una país-Array de Objects
+Object.entries(window.WORLDBANK);
+console.log(Object.entries(window.WORLDBANK));//[[],[],[]...] Array de Arrays cada array tiene 2 elementos el nombre del Pais y el objeto.
+
+
+
+
+
+
+
+
+
+
+
+
+//Creando un arra y de arrays de toda la data de worldbank  Cada array contiene un Object
+const arrayOfArraysOfObjects=(obj)=>{//{}
+  const arr=[];
+  const properties=Object.keys(obj);
+  for(let i =0;i<properties.length;i++){
+    arr.push([obj[properties[i]]])
+  }
+return arr;
+}
+arrayOfArraysOfObjects(window.WORLDBANK);//[[{}],[{}],[{}]...] Array de Arrays Cada array contiene un Object
+//console.log(arrayOfArraysOfObjects(window.WORLDBANK));//[[{}],[{}],[{}]...] Array de Arrays Cada array contiene un Object
+
+
+//Creando una funcion 
+//Recibe como input un objeto {} window.WORLDBANK
+//Retorna como output un [] array 2d
+//Igual a Object.entries
+const objectToArr=(obj)=>{//{}
+  const newArr=[];
+  const properties=Object.keys(obj);
+  for(let i =0;i<properties.length;i++){
+    newArr.push([properties[i],obj[properties[i]]])
+  }
+return newArr;
+}
+objectToArr(window.WORLDBANK);//[["",{}],["",{},["",{}]..] Array of Arrays c/arraY tiene string (PER,MEX,...)y un objeto
+//console.log(objectToArr(window.WORLDBANK));//[["",{}],["",{},["",{}]..]  Array Of Arrays
+
+
+
+
+
+
+
+
+
 //Creo un {} nuevo donde explico lo que significa cada Inicial
 const initialsDescription = {
   "SL." : 'Laboral',
@@ -20,6 +105,8 @@ const initialsDescription = {
   "IC." : 'Inversion', 
   "DT." : 'Deuda Extena'
 };
+
+
 
 
 //Utilizo el {} creado anteriormente para crear [{},{},{}..] c/u de los {} representa un Indicador y tiene 2 Keys indicador (2 inciales), y descripcion
@@ -44,6 +131,27 @@ const printIndicatorCodesInitialsDescription = (array, domElement) => {//[{},{},
   domElement.innerHTML = string;
  }
  printIndicatorCodesInitialsDescription(arrIndicatorsInitialsAndDescription,selectIndicatorCodeElement);
+
+
+
+//Funcion para Imprimir en el Dom ...
+const printIndicatorCountryName = (arrOb, domElement) => {//[{},{},{}..] c/u de {} representa un Indicador y tiene 2 Keys indicador (2 inciales), y descripcion y DOM element
+  
+
+for(let i=0;i<arrOb.length;i++){//[{},{},{}] array de objects c/u objects representa a un país
+arrOb[i];
+let string = '<option value="Todos">Todos</option>';
+  
+      string += `<option value=${arrOb[i].countrycode}>${arrOb[i].countryName}</option>`
+  
+  domElement.innerHTML = string;
+ }
+}
+
+
+printIndicatorCountryName(objToArrObj(window.WORLDBANK),selectCountryName);
+
+
 
  // FILTER BUTTON
 btnSearch.addEventListener('click', () => {
