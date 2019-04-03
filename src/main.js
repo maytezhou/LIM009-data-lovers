3ef329a192a5a8a4b03563f36354315c10ed3a78
maytezhou <maytezhou14@gmail.com>
2019-04-03 18:07:33 -0500

    agregando opcion de ordenar de manera ascendente y descendente y mostrando y ocultando pantallas

--- src/main.js
+++ src/main.js
@@ -7,7 +7,11 @@ const arrDataWorldBankChile=window.WORLDBANK.CHL.indicators;//[{},{},{}...]
 const arrDataWorldBankBrasil=window.WORLDBANK.BRA.indicators;//[{},{},{}...]
 //console.log(arrDataWorldBankBrasil);
 const arrDataWorldBankPeru = window.WORLDBANK.PER.indicators;//[{},{},{}...]
-
+const firstScreen=document.getElementById("first-screen");
+const secondScreen=document.getElementById("second-screen");
+const thirdScreen=document.getElementById("third-screen");
+secondScreen.style.display='none';
+thirdScreen.style.display='none';
 
 const showInfoData = document.getElementById('show-info-data');
 const showFilteredCategory = document.getElementById('show-filtered-category');
@@ -20,6 +24,7 @@ const showSelectedIndicatorName = document.getElementById('show-selected-indicat
 const showIndicatorNameKeysValues = document.getElementById('show-indicator-name-keys-values');
 const arrUniqueInitialsCode = worldBank.getUniqueInitialsIndicatorCodeValues(worldBank.getInitialsIndicatorCodeValues(arrDataWorldBankPeru));
 // console.log(arrUniqueInitialsCode);//["","",""...]
+
 //Creo un {} nuevo donde explico lo que significa cada Inicial
 const initialsDescription = {
   "SL." : 'Laboral',
@@ -34,7 +39,7 @@ const initialsDescription = {
   "DT." : 'Deuda Extena'
 };
 
-
+//Funcion que no he usado aún-Quizas me pueda servir 
 const obtainingOneIndicatorById=(arrOb,indicatorId)=>{//[{},{}] y indicatorId
   let str="";
   for(let i=0;i<arrOb.length;i++){
@@ -72,8 +77,8 @@ const printIndicatorCodesInitialsDescription = (array, domElement) => {//[{},{},
  }
  printIndicatorCodesInitialsDescription(arrIndicatorsInitialsAndDescription,selectIndicatorCodeElement);
  
-
- const orderIndicatorNamesByAlphabet=(arrObject,userSortOrder)=>{
+//Funcion  sort creada para ordenar nombre de indicadores alfabeticamente A-Z o Z-A
+ const orderIndicatorNamesByAlphabet=(arrObject,userSortOrder)=>{//[{},{},{}] y value de la opcion seleccionada por el usuario
   if(userSortOrder==="ascendant"){
   arrObject.sort(function (a,b) {
     return a.indicatorName.localeCompare(b.indicatorName)})
@@ -87,85 +92,58 @@ else if(userSortOrder==="descendant"){
 
 
 
- // FILTER BUTTON
+
+const showTitle=document.getElementById("show-title");
+const orderSelector=document.getElementById("order-selectors");
+
+ // FILTER BUTTON es un  boton dentro de la primera pantalla
 btnSearch.addEventListener('click', () => {
+  firstScreen.style.display='none';
+  secondScreen.style.display='block';
   const filteredIndicatorsByCategory = worldBank.filterBySector(arrDataWorldBankPeru, selectIndicatorCodeElement.value);//[{},{},{}..] Array de Indicators By Category
-  
+  console.log(filteredIndicatorsByCategory);
   console.log(selectIndicatorCodeElement.value);
+  let titleOfIndicatorsList='';
+  let indicatorsList='';
   
-  showInfoData.innerHTML = '';
-  for (let i = 0; i < filteredIndicatorsByCategory.length; i++) {
-    showInfoData.innerHTML =
+  for (let i = 0; i<filteredIndicatorsByCategory.length; i++) {
+    titleOfIndicatorsList=
     `
     <h2>Indicadores de ${filteredIndicatorsByCategory[i].countryName} según categoría : ${initialsDescription[selectIndicatorCodeElement.value]}</h2>
     `
-    for (let j = 0; j < filteredIndicatorsByCategory.length; j++){
-      showInfoData.innerHTML +=
+  }
+  
+    for (let j = 0;j <filteredIndicatorsByCategory.length; j++){
+      indicatorsList+=
         `
         <ul><a href="#"><li id="${filteredIndicatorsByCategory[j].indicatorCode}" class="list">${filteredIndicatorsByCategory[j].indicatorName} en ${filteredIndicatorsByCategory[j].countryName}.</li></a></ul>
         `
     }
-  }console.log(orderIndicatorNamesByAlphabet(filteredIndicatorsByCategory,"descendant"));
+    showTitle.innerHTML=titleOfIndicatorsList;
+    showInfoData.innerHTML=indicatorsList;
+    const listOfIndicatorsShowed=document.querySelectorAll('li.list');
+   console.log(listOfIndicatorsShowed);
+   
+   orderSelector.addEventListener('change',(e)=>{
+   let valueSelected=e.target.value;
+   console.log(e.target.value);
+   orderIndicatorNamesByAlphabet(filteredIndicatorsByCategory,valueSelected);
+   console.log(orderIndicatorNamesByAlphabet(filteredIndicatorsByCategory,valueSelected));
+   
+
+}
+
+);
+
+
 });
 
-const listOfIndicatorsShowed=document.querySelectorAll('li.list');
-console.log(listOfIndicatorsShowed);
 
-
-
-
-/*const orderIndicatorByIndicatorName = (arrObj) => {//[{},{},{}]
-  let orderByIndicatorName = arrObj.sort((a,b) => {
-    indicatorA=a.indicatorName.toUpperCase();
-    indicatorB=b.indicatorName.toUpperCase();
-    return (indicatorA<indicatorB)?-1:(indicatorA>indicatorB)?1:0;
-  });
-  return orderByIndicatorName; //[{},{},{}]
-};
-console.log(orderIndicatorByIndicatorName(arrDataWorldBankPeru));//[{},{},{}]*/
-
-/*const orderIndicatorByIndicatorCode = (arrObj) => {//[{},{},{}]
-  let orderByIndicatorCode = arrObj.sort((a,b) => {
-    indicatorA=a.indicatorCode.toUpperCase();
-    indicatorB=b.indicatorCode.toUpperCase();
-    return (indicatorA<indicatorB)?-1:(indicatorA>indicatorB)?1:0;
-  });
-  return orderByIndicatorCode; //[{},{},{}]
-};
-console.log(orderIndicatorByIndicatorCode(arrDataWorldBankPeru));//[{},{},{}]*/
-
-
-
-
-
-
-
-
-//Ordena de A -Z
-/*arrDataWorldBankPeru.sort(function (a, b) {
-  return a.indicatorName.localeCompare(b.indicatorName);
-});
-console.log(arrDataWorldBankPeru.sort(function (a, b) {
-  return a.indicatorName.localeCompare(b.indicatorName);
-}));*/
-
-/*Ordena de Z-A
-arrDataWorldBankPeru.sort(function (b,a) {
-  return a.indicatorName.localeCompare(b.indicatorName);
-});
-console.log(arrDataWorldBankPeru.sort(function (b,a) {
-  return a.indicatorName.localeCompare(b.indicatorName);
-}));*/
-
-
-
-
-
-
-
-
-// MOSTRAR NOMBRE DE CATEGORÍA SELECCIONADO EN LA SEGUNDA PANTALLA
+// MOSTRAR  LA LISTA CON LOS NOMBRES DE INDICADORES FILTRADOS POR CATEGORIA  EN LA SEGUNDA PANTALLA
+//ShowInfoData es un div dentro de la segunda pantalla
 showInfoData.addEventListener('click', (e) =>{
+  secondScreen.style.display='none';
+  thirdScreen.style.display='block';
   console.log(e.target); 
   showSelectedIndicatorName.innerHTML =
   `
@@ -203,5 +181,47 @@ showInfoData.addEventListener('click', (e) =>{
     }
   };
 });
+
+
+
+//La lógica para crear la funcion orderIndicatorNamesByAlphabet
+//Ordena de A -Z
+/*arrDataWorldBankPeru.sort(function (a, b) {
+  return a.indicatorName.localeCompare(b.indicatorName);
+});
+console.log(arrDataWorldBankPeru.sort(function (a, b) {
+  return a.indicatorName.localeCompare(b.indicatorName);
+}));*/
+
+/*Ordena de Z-A
+arrDataWorldBankPeru.sort(function (b,a) {
+  return a.indicatorName.localeCompare(b.indicatorName);
+});
+console.log(arrDataWorldBankPeru.sort(function (b,a) {
+  return a.indicatorName.localeCompare(b.indicatorName);
+}));*/
+
+
+/*const orderIndicatorByIndicatorName = (arrObj) => {//[{},{},{}]
+  let orderByIndicatorName = arrObj.sort((a,b) => {
+    indicatorA=a.indicatorName.toUpperCase();
+    indicatorB=b.indicatorName.toUpperCase();
+    return (indicatorA<indicatorB)?-1:(indicatorA>indicatorB)?1:0;
+  });
+  return orderByIndicatorName; //[{},{},{}]
+};
+console.log(orderIndicatorByIndicatorName(arrDataWorldBankPeru));//[{},{},{}]*/
+
+/*const orderIndicatorByIndicatorCode = (arrObj) => {//[{},{},{}]
+  let orderByIndicatorCode = arrObj.sort((a,b) => {
+    indicatorA=a.indicatorCode.toUpperCase();
+    indicatorB=b.indicatorCode.toUpperCase();
+    return (indicatorA<indicatorB)?-1:(indicatorA>indicatorB)?1:0;
+  });
+  return orderByIndicatorCode; //[{},{},{}]
+};
+console.log(orderIndicatorByIndicatorCode(arrDataWorldBankPeru));//[{},{},{}]*/
+
+
   
   
